cd5c6b7e006e74fb8d59f321ac759de27d4048f7
Justus Languell <justus@tamu.edu>
2025-08-29 19:11:54 -0500

    Trying many faults

--- Core/Src/dbms/fault_handler.h
+++ Core/Src/dbms/fault_handler.h
@@ -1,8 +1,8 @@
 //  
 //  Copyright (c) Texas A&M University.
 //  
-#ifndef _FAULT_HANDLER_H_
-#define _FAULT_HANDLER_H_
+#ifndef _CTRL_FAULT_HANDLER_H_
+#define _CTRL_FAULT_HANDLER_H_
 
 #include "common.h"
 #include "context.h"
@@ -11,27 +11,65 @@
  *  Fault types, ordinal determines position in bitmask
  */
 typedef enum {
-    FAULT_VOLTAGE_OVER = 0,      
-    FAULT_VOLTAGE_UNDER = 1,     
-    FAULT_TEMP_OVER = 2,         
-    FAULT_TEMP_UNDER = 3,        
-    FAULT_CURRENT_OVER = 4,      
-    FAULT_CURRENT_UNDER = 5,     
-    FAULT_PACK_VOLTAGE_OVER = 6, 
-    FAULT_PACK_VOLTAGE_UNDER = 7,
-    FAULT_TYPE_COUNT             // Total number of fault types -- should be last
-} FaultType;
+    CTRL_FAULT_VOLTAGE_OVER = 0,      
+    CTRL_FAULT_VOLTAGE_UNDER = 1,     
+    CTRL_FAULT_TEMP_OVER = 2,         
+    CTRL_FAULT_TEMP_UNDER = 3,        
+    CTRL_FAULT_CURRENT_OVER = 4,      
+    CTRL_FAULT_CURRENT_UNDER = 5,     
+    CTRL_FAULT_PACK_VOLTAGE_OVER = 6, 
+    CTRL_FAULT_PACK_VOLTAGE_UNDER = 7,
+    CTRL_FAULT_TYPE_COUNT             // Total number of fault types -- should be last
+} ControllerFaultType;
 
-void SetFault(DbmsCtx* ctx, FaultType fault);
-void ClearFault(DbmsCtx* ctx, FaultType fault);
-bool HasFault(DbmsCtx* ctx, FaultType fault);
-#define HAS_ANY_FAULTS(CTX) (((DbmsCtx*)CTX)->fault_mask != 0)
-#define CLEAR_ALL_FAULTS(CTX) (((DbmsCtx*)CTX)->fault_mask = 0)
+typedef enum {
+    STACK_FAULT_CONF_MON_ERR = 0,
+    STACK_FAULT_FACTLDERR,
+    STACK_FAULT_FACT_CRC,
+    STACK_FAULT_VALIDATE_DET,
+    STACK_FAULT_LFO,
+    STACK_FAULT_SHUTDOWN_REC,
+    STACK_FAULT_DRST,
+    STACK_FAULT_CTL,
+    STACK_FAULT_CTS,
+    STACK_FAULT_TSHUT,
+    STACK_FAULT_INH,
+    STACK_FAULT_CVDD_UV_DRST,
+    STACK_FAULT_CVDD_OV,
+    STACK_FAULT_DVDD_OV,
+    STACK_FAULT_AVDDREF_OV,
+    STACK_FAULT_FCOMM_DET,
+    STACK_FAULT_FTONE_DET,
+    STACK_FAULT_HB_FAIL,
+    STACK_FAULT_HB_FAST,
+    STACK_FAULT_UART_FRAME,
+    STACK_FAULT_COMMCRL_DET,
+    STACK_FAULT_STOP_DET,
+    STACK_FAULT_AVAO_SW_FAIL,
+    STACK_FAULT_SPI_FRAME,
+    STACK_FAULT_SPI_PHY,
+    STACK_FAULT_COML_FRAME,
+    STACK_FAULT_COML_PHY,
+    STACK_FAULT_COMH_FRAME,
+    STACK_FAULT_COMH_PHY,
+    STACK_FAULT_TYPE_COUNT             // Total number of fault types -- should be last
+} StackFaultType;
 
-// Fault checking functions
+
+void ControllerSetFault(DbmsCtx* ctx, ControllerFaultType fault);
+void ControllerClearFault(DbmsCtx* ctx, ControllerFaultType fault);
+bool ControllerHasFault(DbmsCtx* ctx, ControllerFaultType fault);
+
 void CheckVoltageFaults(DbmsCtx* ctx);
 void CheckTemperatureFaults(DbmsCtx* ctx);
 void CheckCurrentFaults(DbmsCtx* ctx);
+
+void StackSetFault(DbmsCtx* ctx, uint8_t addr, StackFaultType fault);
+void StackClearFault(DbmsCtx* ctx, uint8_t addr, StackFaultType fault);
+bool StackHasFault(DbmsCtx* ctx, uint8_t addr, StackFaultType fault);
+
+bool HasAnyFaults(DbmsCtx* ctx);
+void ClearAllFaults(DbmsCtx* ctx);
 
 // Comm to the GPIO
 void ThrowHardFault(DbmsCtx* ctx);
