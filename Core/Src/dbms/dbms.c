28ab1cacb18271a71c1a716eb4a4a4bdc24dcd0a
abhinav akavaram <abhinav.akavaram@tamu.edu>
2025-09-07 16:21:24 -0500

    loop iter edits

--- Core/Src/dbms/dbms.c
+++ Core/Src/dbms/dbms.c
@@ -166,17 +166,21 @@ void DbmsIter(DbmsCtx* ctx)
         // Need to look into this
         // todo: will time out a few times before stack is 
         //       correctly configed, fix this
-        StackUpdateVoltReadings(ctx);
-        HAL_Delay(8);
-        StackUpdateTempReadings(ctx);
-        HAL_Delay(8);
+        if (ctx->stats.iters % 10 == 0){
+            StackUpdateTempReadings(ctx);
+            // HAL_Delay(8);
+        }
+        else if (ctx->stats.iters % 10 != 1){
+            StackUpdateVoltReadings(ctx);
+            // HAL_Delay(8);
+        }
     }
 
     // if (PERIOD(ctx->stats.iters, 1, 0)) //todo: fix ts
     SendCellVoltages(ctx);
-    HAL_Delay(8);
+//    HAL_Delay(8);
     SendCellTemps(ctx);
-    HAL_Delay(8);
+//    HAL_Delay(8);
     SendMetrics(ctx);
 
     //
@@ -197,7 +201,7 @@ void DbmsIter(DbmsCtx* ctx)
    MonitorLedBlink(ctx);
     // *((uint32_t*)wrap_queue_push(&ctx->stats.looptimes_q)) = ctx->iter_end_us - ctx->iter_start_us;
     // DelayUs(ctx, end_delay);
-    HAL_Delay(20);  // ^ todo: fix all this
+    HAL_Delay(100);  // ^ todo: fix all this
 }
 
 
