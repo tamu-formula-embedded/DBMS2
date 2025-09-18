0a82c1718dc0990e12e2d0fad4bbcc2bc95d4980
Justus Languell <justus@tamu.edu>
2025-09-17 20:58:08 -0500

    FML

--- Core/Src/dbms/data.h
+++ Core/Src/dbms/data.h
@@ -5,18 +5,16 @@
 
 #include "lut.h"
 
-#define N_THERM_V_TO_T_ENTRIES      121
-#define N_OCV_ENTRIES               201
-#define N_RC_ENTRIES                101
-#define N_C_ENTRIES                 101
-
-extern const LTE lut_therm_v_to_t[N_THERM_V_TO_T_ENTRIES];
-void MakeLutThermistors();
 
 extern const float ocv_t_high[N_OCV_ENTRIES];
 extern const float ocv_t_low[N_OCV_ENTRIES];
 
 extern const float rc_t_high[3][N_RC_ENTRIES];
 extern const float rc_t_low[3][N_RC_ENTRIES];
+
+
+void DataInit(DbmsCtx* ctx);
+
+float ThermVoltToTemp(DbmsCtx* ctx, float V);
 
 #endif
