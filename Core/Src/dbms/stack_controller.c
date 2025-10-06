55e6df091074ae570a722e961d84df860690b811
Joshua Wu <joshuawu158@gmail.com>
2025-10-05 23:02:14 -0500

    Gud?

--- Core/Src/dbms/stack_controller.c
+++ Core/Src/dbms/stack_controller.c
@@ -387,30 +387,61 @@ void StackUpdateVoltReadingSingle(DbmsCtx* ctx, uint16_t addr)
     uint8_t FRAME_ADC_MEASUREMENTS[] = {0x80, in_addr & 0xff, 0x05, 0x68 + 2 * (16 - N_GROUPS_PER_SIDE), N_GROUPS_PER_SIDE * 2 - 1,
                                         0x00, 0x00};
 
-    if ((status = SendStackFrameSetCrc(ctx, FRAME_ADC_MEASUREMENTS, sizeof(FRAME_ADC_MEASUREMENTS))) != 0)
-    {
-        CAN_REPORT_FAULT(ctx, status);
-    }
-
     size_t data_size = N_GROUPS_PER_SIDE * sizeof(int16_t);
     size_t expected_rx_size = RX_FRAME_SIZE(data_size) * 1; // <- num frames
     //                 header+crc ^         ^ readings * 2 bytes each
 
-    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_buffer_volt_readings, expected_rx_size, STACK_RECV_TIMEOUT)) != 0)
+    if ((status = SendStackFrameSetCrc(ctx, FRAME_ADC_MEASUREMENTS, sizeof(FRAME_ADC_MEASUREMENTS))) != 0)
     {
-        
     }
+   
+    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_buffer_volt_readings, expected_rx_size+2, 10)) != 0)
+    {
+    } 
+
     ctx->stats.n_rx_stack_frames++;
 
-    RxStackFrame rx_frame = {0};
-    FillStackFrame(&rx_frame, rx_buffer_volt_readings, data_size);
-    if (rx_frame.dev_addr != in_addr) return;
-    if (rx_frame.crc != CalcStackFrameCrc(&rx_frame)) {
-        ctx->stats.n_rx_stack_bad_crcs++;
-        return;
+
+    uint8_t* data = rx_buffer_volt_readings;
+    while (data[0] != 0x6C) data++;
+    data++;
+    uint16_t f_crc = (data[data_size]) + (data[data_size+1] << 8);
+    *(data-1) = 0x6C;
+    *(data-2) = 0x05;
+    *(data-3) = in_addr;
+    *(data-4) = 0x1B;
+    uint16_t c_crc = CalcCrc16(data-4, data_size+4);
+    // CanLog(ctx, "V %d\n", data[0]);
+    if (f_crc != c_crc) return;
+
+    for (size_t j = 0; j < N_GROUPS_PER_SIDE; j++)
+    {
+        uint16_t raw =
+                (data[j * sizeof(int16_t)] << 8) + (data[j * sizeof(int16_t) + 1]);
+
+        ctx->cell_states[addr].voltages[j] = (raw * STACK_V_UV_PER_BIT) / 1000.0; // floating mV
     }
 
-    // CanLog(ctx, "V %d %d\n", in_addr, (rx_frame.data[0] << 8) + rx_frame.data[1]);
+#ifndef OLDSCHOOL
+    RxStackFrame rx_frame = {0};
+    FillStackFrame(&rx_frame, rx_buffer_volt_readings, data_size);
+
+    // What the fuck
+    if (rx_frame.data[0] == 0x6C)
+        rx_frame.data++;
+    *(rx_frame.data-1) = 0x6C;
+    *(rx_frame.data-2) = 0x05;
+    *(rx_frame.data-3) = in_addr;
+    *(rx_frame.data-4) = 0x1B;
+    // end what the fuck
+
+    // CanLog(ctx, "V %x\n", rx_frame.data[0]);
+    // if (rx_frame.dev_addr != in_addr) return;
+    if (rx_frame.crc != CalcStackFrameCrc(&rx_frame)) {
+        // ctx->stats.n_rx_stack_bad_crcs++;
+        // CanLog(ctx, "U %d\n", in_addr);
+        return;
+    }
 
      for (size_t j = 0; j < N_GROUPS_PER_SIDE; j++)
     {
@@ -419,8 +450,7 @@ void StackUpdateVoltReadingSingle(DbmsCtx* ctx, uint16_t addr)
 
         ctx->cell_states[addr].voltages[j] = (raw * STACK_V_UV_PER_BIT) / 1000.0; // floating mV
     }
-
-
+#endif
 }
 
 // votlages ->  if (addr % 2 == 0)
@@ -446,7 +476,7 @@ void StackUpdateTempReadings(DbmsCtx* ctx)
     size_t expected_rx_size = RX_FRAME_SIZE(data_size) * N_MONITORS;
     static uint8_t rx_frame[1024 * 4];
 
-    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_frame, expected_rx_size, expected_rx_size)) != 0)
+    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_frame, expected_rx_size, 10)) != 0)
     {
         //    CAN_REPORT_FAULT(ctx, status);
         // CanLog(ctx, "Wtf %d\n", status);
