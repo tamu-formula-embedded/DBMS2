56fcd17a3e025f4ef6442d6ed23baf1640d86864
Joshua Wu <joshuawu158@gmail.com>
2025-10-30 20:22:52 -0500

    Ch stuff

---
+++ Core/Src/dbms/charging.c
@@ -0,0 +1,18 @@
+#include "charging.h"
+
+void ChargingEnter(DbmsCtx* ctx)
+{
+    // set to balancing for now
+    ctx->charging.state = CH_BALANCING;
+}
+
+void ChargingUpdate(DbmsCtx* ctx)
+{
+
+}
+
+void ChargingExit(DbmsCtx* ctx)
+{
+
+}
+
