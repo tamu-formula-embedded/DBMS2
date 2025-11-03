af2f0d3d053f8dadbf3c98e250f7e8114e4abb53
Justus Languell <justus@tamu.edu>
2025-11-03 14:38:52 -0600

    Rm debug

--- Debug/Core/Src/lib/lib.d
+++
@@ -1,7 +0,0 @@
-Core/Src/lib/lib.o: ../Core/Src/lib/lib.c ../Core/Src/lib/lib.h \
- ../Core/Src/lib/crc.h ../Core/Src/lib/wrap_q.h \
- ../Core/Src/lib/lookup_table.h
-../Core/Src/lib/lib.h:
-../Core/Src/lib/crc.h:
-../Core/Src/lib/wrap_q.h:
-../Core/Src/lib/lookup_table.h:
--- Sim/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf src/dbms
-rm -rf src/lib
-cp -r ../Core/Src/dbms src/dbms
-cp -r ../Core/Src/lib src/lib
-make
-rm -rf src/dbms
-rm -rf src/lib
