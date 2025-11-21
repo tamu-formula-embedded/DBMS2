953e5d6d6ce5784e672ce5b252d53d1a9ff38f98
abhinav-092 <abhinav.akavaram@tamu.edu>
2025-11-20 18:50:07 -0600

    moved stuff from old branch into here, ready to test

--- Core/Src/dbms/stack.c
+++ Core/Src/dbms/stack.c
@@ -244,6 +244,47 @@ void StackUpdateVoltReadingSingle(DbmsCtx* ctx, uint16_t addr)
     }
 }
 
+void StackUpdateAllVoltReadings(DbmsCtx* ctx)
+{
+    int status = 0;
+    static uint8_t rx_buffer_v[1024];
+
+    uint8_t frame[] = {0xB0, 0x05, 0x68 + 2 * (16 - N_GROUPS_PER_SIDE), N_GROUPS_PER_SIDE * 2 - 1, 0x00, 0x00};
+
+    size_t data_size = N_GROUPS_PER_SIDE * sizeof(int16_t);
+    size_t expected_rx_size = RX_FRAME_SIZE(data_size) * N_MONITORS;
+
+    if ((status = SendStackFrameSetCrc(ctx, frame, sizeof(frame))) != 0) { }
+    // TODO: what the fuck is the +2 for?
+    // TODO: redo the RX path for stack
+    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_buffer_v, expected_rx_size, STACK_RECV_TIMEOUT)) != 0) { } 
+    for (int i = 0; i < N_MONITORS; i++)
+    {
+        ctx->stats.n_rx_stack_frames++;
+
+        uint8_t* data = rx_buffer_v + (i * RX_FRAME_SIZE(data_size));
+        while (data[0] != frame[2]) data++;
+        data++;
+        uint16_t f_crc = (data[data_size]) + (data[data_size+1] << 8);
+        *(data-1) = frame[2];
+        *(data-2) = frame[1];
+        *(data-3) = i + 1;
+        *(data-4) = frame[3];
+        uint16_t c_crc = CalcCrc16(data-4, data_size+4);
+        if (f_crc != c_crc) 
+        {
+            ctx->stats.n_rx_stack_bad_crcs++;
+            return;
+        }
+
+        for (size_t j = 0; j < N_GROUPS_PER_SIDE; j++)
+        {
+            uint16_t raw = (data[j * sizeof(int16_t)] << 8) + (data[j * sizeof(int16_t) + 1]);
+            ctx->cell_states[i].voltages[j] = (raw * STACK_V_UV_PER_BIT) / 1000.0; // floating mV
+        }
+    }
+}
+
 /**
  * @brief Configure GPIOs for temp readings
  * 
@@ -306,6 +347,47 @@ void StackUpdateTempReadingSingle(DbmsCtx* ctx, uint16_t addr, bool sidekick)
     {
         uint16_t raw = (data[j * sizeof(int16_t)] << 8) + (data[j * sizeof(int16_t) + 1]);
         ctx->cell_states[addr].temps[j + offset] = ThermVoltToTemp(ctx, MAX(0, (raw * STACK_T_UV_PER_BIT) / 1000000.0));
+    }
+}
+
+void StackUpdateAllTempReadings(DbmsCtx* ctx)
+{
+    int status = 0;
+    static uint8_t rx_buffer_t[1024];
+
+    uint8_t frame[] = {0xB0, 0x05, 0x8E, N_TEMPS_PER_MONITOR, 0x00, 0x00};
+
+    size_t data_size = N_TEMPS_PER_MONITOR * sizeof(int16_t);
+    size_t expected_rx_size = RX_FRAME_SIZE(data_size) * N_MONITORS;
+
+    if ((status = SendStackFrameSetCrc(ctx, frame, sizeof(frame))) != 0) { }
+    // TODO: what the fuck is the +2 for?
+    // TODO: redo the RX path for stack
+    if ((status = HAL_UART_Receive(ctx->hw.uart, rx_buffer_t, expected_rx_size, STACK_RECV_TIMEOUT)) != 0) { } 
+    for (int i = 0; i < N_MONITORS; i++)
+    {
+        ctx->stats.n_rx_stack_frames++;
+
+        uint8_t* data = rx_buffer_t + (i * RX_FRAME_SIZE(data_size));
+        while (data[0] != frame[2]) data++;
+        data++;
+        uint16_t f_crc = (data[data_size]) + (data[data_size+1] << 8);
+        *(data-1) = frame[2];
+        *(data-2) = frame[1];
+        *(data-3) = i + 1;
+        *(data-4) = frame[3];
+        uint16_t c_crc = CalcCrc16(data-4, data_size+4);
+        if (f_crc != c_crc) 
+        {
+            ctx->stats.n_rx_stack_bad_crcs++;
+            return;
+        }
+        uint8_t offset = (i + 1) % 2 == 0 ? N_TEMPS_PER_MONITOR : 0;
+        for (size_t j = 0; j < N_TEMPS_PER_MONITOR; j++)
+        {
+            uint16_t raw = (data[j * sizeof(int16_t)] << 8) + (data[j * sizeof(int16_t) + 1]);
+            ctx->cell_states[i].temps[j + offset] = ThermVoltToTemp(ctx, MAX(0, (raw * STACK_T_UV_PER_BIT) / 1000000.0));
+        }
     }
 }
 
