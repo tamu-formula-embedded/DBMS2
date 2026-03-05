eb2ae643ff17efcb6883249b9249de3558ea951e
Juicestus <jus@gtsbr.org>
2026-03-05 00:04:45 -0600

    More ideas++

--- idea2.c
+++ idea2.c
@@ -1,17 +1,35 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define N_GROUPS_PER_SIDE 1
+
+#define N_SEGMENTS          1       // number of segments in the stack
+#define N_SIDES_PER_SEG     2       // number of sides per segment
+#define N_MONITORS_PER_SIDE 1       // number of monitors per side
+#define N_GROUPS_PER_SIDE   13      // number of voltages per side
+#define N_TEMPS_PER_MONITOR 13       // number of temps per monitor chip 
+#define N_P_GROUP           3       // number of cells per parallel group
+
+#define N_TEMPS_PER_SIDE (N_MONITORS_PER_SIDE * N_TEMPS_PER_MONITOR)
+#define N_SIDES (N_SEGMENTS * N_SIDES_PER_SEG)
+#define N_MONITORS (N_SEGMENTS * N_SIDES_PER_SEG * N_MONITORS_PER_SIDE)
+#define N_STACKDEVS (N_MONITORS + 1) // technically "bus devs"
 
 // External dependencies
 typedef struct {
     struct { int uart; } hw;
+    struct { int n_rx_stack_frames, n_rx_stack_bad_crcs; } stats;
+    struct 
+    {
+        float voltages[N_GROUPS_PER_SIDE];
+        float temps[N_TEMPS_PER_SIDE];
+        bool cells_to_balance[N_GROUPS_PER_SIDE];  
+    } cell_states[N_SIDES];
 } DbmsCtx;
+
 uint16_t CalcCrc(uint8_t*, int);
 int HAL_UART_Transmit(int, char*, int, int);
-#define N_MONITORS 2
-#define N_GROUPS_PER_SIDE 13
 
 
 /**
@@ -48,6 +66,9 @@ int HAL_UART_Transmit(int, char*, int, int);
 
 #define STACK_SEND_TIMEOUT  10
 #define STACK_RECV_TIMEOUT  10
+
+#define STACK_V_UV_PER_BIT 190.73
+#define STACK_T_UV_PER_BIT 152.59
 
 /**
  * Important data
@@ -111,10 +132,11 @@ typedef struct PACKED _RxStackFrame
     .data    = { __VA_ARGS__ },                             \
     .__crc   = 0                                            \
 })
+#define DATA(...) __VA_ARGS__
 // example: 
 static uint8_t frame1[] = {0xB2, 0x00, 0x0E, 0x09, 0x2D, 0x09, 0x00, 0x00};
 // becomes:
-static TxStackFrame frame2 = MAKE_TX_FRAME(REQ_STACK_WRITE, 0, 0x0E09, /*data=*/ 0x2D, 0x09 );
+static TxStackFrame frame2 = MAKE_TX_FRAME(REQ_STACK_WRITE, 0, 0x0E09, DATA(0x2D, 0x09) );
 
 
 /**
@@ -136,18 +158,57 @@ int StackSetupGpio(DbmsCtx* ctx)
     SendStackFrame(ctx, MAKE_TX_FRAME(REQ_STACK_WRITE, 0x03, 0x0A01));
 }
 
+int ReadStackFrame(DbmsCtx* ctx, RxStackFrame* frame, size_t datalen)
+{
+    int status = 0;
+    if ((status = HAL_UART_Receive(ctx->hw.uart, (uint8_t*)frame, 6 + datalen, STACK_RECV_TIMEOUT)) != 0) 
+    { 
+        return 1;
+    }     
+    ctx->stats.n_rx_stack_frames++;
+
+    if (CRC(*frame) != CALC_CRC(*frame))
+    {
+        ctx->stats.n_rx_stack_bad_crcs++;
+        return 1;
+    }
+    return 0;
+}
+
 
 /**
  * For recv we can abstract away a lot of the logic
  */
-
-
-
 void StackUpdateAllVoltReadings(DbmsCtx* ctx)
 {
     int regaddr = (0x68 + 2 * (16 - N_GROUPS_PER_SIDE) << 8) + (N_GROUPS_PER_SIDE * 2 - 1);
     SendStackFrame(ctx, MAKE_TX_FRAME(REQ_STACK_READ, 0x05, regaddr));
 
-    RxStackFrame rxframe = {0};
-    
+    // // can we read segment by segment? or do we have to read them all at once 
+    // for (int i = 0; i < N_MONITORS; i++)
+    // {
+    //     RxStackFrame frame;
+    //     ReadStackFrame(ctx, &frame, N_GROUPS_PER_SIDE * sizeof(uint16_t));
+
+    //     uint16_t* voltages = (uint16_t*)(frame.data);   
+    //     for (size_t j = 0; j < N_GROUPS_PER_SIDE; j++)
+    //     {
+    //         uint16_t raw = ESWAP16(voltages[j]);
+    //         ctx->cell_states[frame.devaddr / 2].voltages[j] = (raw * STACK_V_UV_PER_BIT) / 1000.0; // floating mV
+    //     }
+    // }
+
+    // if we read them all at once:
+    uint8_t raw[4096];
+    HAL_Read(raw);      // read into the buffer
+    int offset = 0;
+    for (int i = 0; i < N_MONITORS; i++)
+    {
+        RxStackFrame* frame = raw[offset];
+        // CRC check
+        ReadStackFrame(ctx, &frame, N_GROUPS_PER_SIDE * sizeof(uint16_t));
+        offset += FRAME_LEN(frame);
+
+
+    // You get the idea... prob need some little code edits here and there
 }
