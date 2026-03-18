9e89f3389d641843e8df6988308cc129e02165dc
Eli Nicksic <elinicksic@gmail.com>
2026-03-18 03:35:27 -0500

    Precharge min voltage

--- Core/Src/dbms/precharge.c
+++ Core/Src/dbms/precharge.c
@@ -5,7 +5,6 @@
  * Copyright (C) 2025   Texas A&M University
  * 
  *                      Justus Languell  <justus@tamu.edu>
- *                      Cam Stone        <cameron28202@tamu.edu>
  *                      Abhinav Akavaram <abhinav.akavaram@tamu.edu>
  *                      Eli Nicksic      <eli.n@tamu.edu>
  */
@@ -23,7 +22,7 @@ void PrechargeUpdate(DbmsCtx* ctx)
   float pack_mv = ctx->stats.pack_v;
   float threshold = 0.9; //GetSetting(ctx, ctx->precharged ? PRECHARGE_OFF_TH : PRECHARGE_ON_TH);
  
-  ctx->precharged = (ivt_mv > pack_mv * threshold) && !ctx->stats.fault_line_faulted;
+  ctx->precharged = pack_mv > 50000 && (ivt_mv > pack_mv * threshold) && !ctx->stats.fault_line_faulted ;
  
   PrechargeSet(ctx);
 }
