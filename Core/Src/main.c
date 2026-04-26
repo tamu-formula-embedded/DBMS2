b7b5583852d168c7f90e3ce9bc726123d3a18289
Juicestus <jus@gtsbr.org>
2026-04-26 04:35:43 -0500

    Charging stuff

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -292,9 +292,14 @@ static void MX_CAN2_Init(void)
 
   /* USER CODE BEGIN CAN2_Init 1 */
 
+  // hcan2.Init.Prescaler | Baud Rate
+  // ---------------------+----------
+  // 6                    | 1M
+  // 12                   | 500K
+
   /* USER CODE END CAN2_Init 1 */
   hcan2.Instance = CAN2;
-  hcan2.Init.Prescaler = 6;
+  hcan2.Init.Prescaler = 12;
   hcan2.Init.Mode = CAN_MODE_NORMAL;
   hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan2.Init.TimeSeg1 = CAN_BS1_5TQ;
